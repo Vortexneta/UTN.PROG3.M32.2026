ada95ecf574e29209f3bb2f657931f0de865d539
Osvaldo Cantone <ocantone@gmail.com>
2026-08-18 12:32:39 -0300

    Clase 3 Tipos en JS

--- app/Clase3/typeof.js
+++ app/Clase3/typeof.js
@@ -3,16 +3,15 @@ let x = 20000;
 console.log("Valor:", x, "| Tipo:", typeof x); 
 // Output: Valor: 10 | Tipo: number
 
-// 2. Reasignación con otro tipo de dato (Imposible en C#)
+// 2. Reasignación con otro tipo de dato (Imposible en C ó C#)
 x = "Ahora soy una cadena";
 console.log("Valor:", x, "| Tipo:", typeof x); 
 // Output: Valor: Ahora soy una cadena | Tipo: string
 
 // 3. Operaciones raras por coerción implícita de tipos
-let resultado = "5" + 2; 
-console.log("Resultado '5' + 2:", resultado, "| Tipo:", typeof resultado); 
-// Output: Resultado '5' + 2: 52 | Tipo: string
+let resultado1 = "5" + 2;
+let resultado2 = "5" - 2;
+console.log(""); 
 
-let resta = "5" - 2;
-console.log("Resultado '5' - 2:", resta, "| Tipo:", typeof resta); 
-// Output: Resultado '5' - 2: 3 | Tipo: number
+console.log("Resultado '5' + 2:", resultado1, "| Tipo:", typeof resultado1); 
+console.log("Resultado '5' - 2:", resultado2, "| Tipo:", typeof resultado2); 
